cf210ccc83ff60c049bc8a2fc74a7b2680be4a97
Vadym Brianka <briankavadym12679@gmail.com>
2025-09-08 11:59:49 +0300

    feat: calculation of the sum of numbers and output of the result have been added

--- lab_1/lab_1/Program.cs
+++ lab_1/lab_1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+
+namespace ConsoleApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            Console.WriteLine("5 + 10 = {0}", x + y);
+            Console.ReadKey();
+        }
+    }
+}
